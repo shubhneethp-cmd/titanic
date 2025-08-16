9ab702e196ff677f760fbed65cccff500123f186
shubhneethp-cmd <shubhneethp@gmail.com>
2025-08-16 07:45:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-(assets/titanic.jpg)
-
+<body style="background: url('assets/titanic.jpg') no-repeat center center fixed; 
+             background-size: cover;">
+  <h1>🚢 Titanic Project</h1>
+</body>
